e9becc0ebe99834dc409d73204837b62e56471a7
Le Hai <le.thanh.hai@sun-asterisk.com>
2024-09-19 10:00:46 +0700

    first commit

---
+++ src/main/java/example/springcashcard/SpringCashCardApplication.java
@@ -0,0 +1,13 @@
+package example.springcashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCashCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCashCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/springcashcard/SpringCashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package example.springcashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
